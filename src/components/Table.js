2289ce01ddd9f8923017d4e8a0946c4a01f7a1a8
leticiaandrade <leticia933@gmail.com>
2022-03-25 04:10:17 -0300

    limpando logs

--- src/components/Table.js
+++ src/components/Table.js
@@ -9,15 +9,14 @@ function Table() {
     .map((planet) => Object.keys(planet).filter((item) => item !== 'residents'));
 
   const tableHeaders = removeResidents[0];
-  // console.log('----------', tableHeaders);
   const residentsIndex = 9;
+  // tentei usar o .entries mas deu muito errado pra resgatar as infos necessarias depois
   /* const planetInfoArr = returnedPlanets.map((item) => Object.entries(item));
   planetInfoArr.map((item) => item.splice(residentsIndex, 1));
   // console.log(planetInfoArr); */
+  // referencia do splice: https://love2dev.com/blog/javascript-remove-from-array/
   const planetInfo = returnedPlanets.map((item2) => Object.values(item2));
-  // console.log('++++++', newArr);
   planetInfo.map((item3) => item3.splice(residentsIndex, 1));
-  // console.log('------', planetInfo);
 
   if (returnedPlanets.length < 1) {
     return (
@@ -42,13 +41,6 @@ function Table() {
               {planet.map((cat) => (<td key={ cat }>{cat}</td>))}
             </tr>
           ))}
-          {/*   <tr>
-            {returnedPlanets.map((planet) => (
-              <td key={ planet.name }>
-                {planet.name}
-              </td>
-            ))}
-          </tr> */}
         </tbody>
       </table>
     </div>
